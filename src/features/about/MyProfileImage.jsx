90ab3e1e52ea19428c78e41b7cab87764b5634b2
ilyana <ilyanamazlan99@gmail.com>
2024-10-30 18:13:31 +0800

    fix: update image path

--- src/features/about/MyProfileImage.jsx
+++ src/features/about/MyProfileImage.jsx
@@ -1,24 +1,29 @@
 import { useState } from "react";
+import meImage from "../../assets/images/me.png";
+import coffee from "../../assets/images/coffee.png";
+import matcha from "../../assets/images/matcha.png";
+import pizza from "../../assets/images/pizza.png";
+import nasiLemak from "../../assets/images/nasi-lemak.png";
 
 function MyProfileImage() {
-  const [image, setImage] = useState("coffee");
+  const [image, setImage] = useState(coffee);
 
   function toggleImage() {
     switch (image) {
-      case "coffee":
-        setImage("matcha");
+      case coffee:
+        setImage(matcha);
         return;
-      case "matcha":
-        setImage("pizza");
+      case matcha:
+        setImage(pizza);
         return;
-      case "pizza":
-        setImage("nasi-lemak");
+      case pizza:
+        setImage(nasiLemak);
         return;
-      case "nasi-lemak":
-        setImage("coffee");
+      case nasiLemak:
+        setImage(coffee);
         return;
       default:
-        setImage("coffee");
+        setImage(coffee);
     }
   }
 
@@ -27,19 +32,14 @@ function MyProfileImage() {
       <div className="absolute bottom-[10%] left-[5%] h-[90%] w-[90%] rounded-full border-4 bg-accentColor"></div>
 
       <div className="relative">
-        <img src={`/src/assets/images/me.png`} alt="My icon" />
+        <img src={meImage} alt="My icon" />
       </div>
 
       <div
         className="image-container absolute left-[60%] top-[65%]"
         onClick={() => toggleImage()}
       >
-        <img
-          src={`/src/assets/images/${image}.png`}
-          width={170}
-          height={170}
-          alt="Snacks"
-        />
+        <img src={image} width={170} height={170} alt="Snacks" />
       </div>
     </div>
   );
